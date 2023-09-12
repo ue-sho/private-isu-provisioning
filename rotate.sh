49f9d310abf8ba2c6eef7992cc70383c0b34f6eb
ue-sho <tssu45@gmail.com>
2023-09-12 22:48:53 +0900

    mysqlもrotateできるようにした

--- rotate.sh
+++ rotate.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# nginx
 sudo -S mv "/var/log/nginx/access.log" "/var/log/nginx/`date +"%Y%m%d%H%M%S"`_access.log"
 sudo -S systemctl restart nginx
+
+# mysql
+sudo -S mv "/var/log/mysql/mysqlslow.log" "/var/log/nginx/`date +"%Y%m%d%H%M%S"`_mysqlslow.log"
+sudo -S systemctl restart mysql
--- rotate_and_cp.sh
+++ rotate_and_cp.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-LOGFILE="`date +"%Y%m%d%H%M%S"`_access.log"
-sudo -S mv "/var/log/nginx/access.log" "/var/log/nginx/$LOGFILE"
-sudo -S cp "/var/log/nginx/$LOGFILE" "log/$LOGFILE"
-sudo -S chown isucon:isucon "log/$LOGFILE"
+# nginx
+NGINX_LOGFILE="`date +"%Y%m%d%H%M%S"`_access.log"
+sudo -S mv "/var/log/nginx/access.log" "/var/log/nginx/$NGINX_LOGFILE"
+sudo -S cp "/var/log/nginx/$NGINX_LOGFILE" "log/nginx/$NGINX_LOGFILE"
+sudo -S chown isucon:isucon "log/nginx/$NGINX_LOGFILE"
 sudo -S systemctl restart nginx
+
+# mysql
+MYSQL_LOGFILE="`date +"%Y%m%d%H%M%S"`_mysqlslow.log"
+sudo -S mv "/var/log/mysql/mysqlslow.log" "/var/log/mysql/$MYSQL_LOGFILE"
+sudo -S cp "/var/log/mysql/$MYSQL_LOGFILE" "log/mysql/$MYSQL_LOGFILE"
+sudo -S chown isucon:isucon "log/mysql/$MYSQL_LOGFILE"
+sudo -S systemctl restart mysql
