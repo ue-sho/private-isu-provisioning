2465d0490dd0f3cb71a4f908dc6cee5480bb63e4
ue-sho <tssu45@gmail.com>
2023-09-14 10:19:10 +0900

    js,css等をnginxで配信する

--- restart.sh
+++ restart.sh
@@ -5,6 +5,8 @@ git pull
 
 # niginx
 sudo cp nginx/nginx.conf /etc/nginx/nginx.conf
+sudo cp nginx/sites-available/isucon.conf /etc/nginx/sites-available/isucon.conf
+
 sudo /usr/sbin/nginx -t  # nginxの設定ファイルの構文チェック
 
 # mysql
@@ -16,6 +18,6 @@ cd golang
 /home/isucon/.local/go/bin/go build -o app
 
 # restart
-sudo systemctl reload nginx
+sudo systemctl restart nginx
 sudo systemctl restart isu-go
 
